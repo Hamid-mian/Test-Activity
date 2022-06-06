5973de1bdc5b081a89a50f98a926b9a4a052e752
Hamid-mian <iqbalhamid380@gmail.com>
2022-06-05 16:48:50 -0800

    Show answer using toast and on click buttons color change and few more functionalities

--- app/src/main/java/com/example/test_activity/Test_Activity.java
+++ app/src/main/java/com/example/test_activity/Test_Activity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import  java.util.Random;
@@ -17,9 +20,18 @@ public class Test_Activity extends AppCompatActivity {
     TextView textView1,textView2,answer;
     Button btn1,btn2,btn3,btn4,submit,next;
     int sum=0,count=0,total=1;
+    TextView myMessage;
+    Toast myToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View myLayout=layoutInflater.inflate(R.layout.toast_layout,null);
+        myMessage= myLayout.findViewById(R.id.textView);
+        myToast=new Toast(getApplicationContext());
+        myToast.setDuration(Toast.LENGTH_LONG);
+        myToast.setView(myLayout);
         setContentView(R.layout.activity_test_avtivity);
         Random rand1 =new Random();
         Random rand2 =new Random();
@@ -27,6 +39,9 @@ public class Test_Activity extends AppCompatActivity {
         int random1= rand1.nextInt(upper_bound1);
         int random2= rand2.nextInt(upper_bound2);
         int random3= rand2.nextInt(upper);
+
+
+
         next=findViewById(R.id.Next_Button1);
         textView1=findViewById(R.id.question_1);
         textView2=findViewById(R.id.question_2);
@@ -38,6 +53,12 @@ public class Test_Activity extends AppCompatActivity {
         textView1.setText(Integer.toString(random1));
         textView2.setText(Integer.toString(random2));
         sum=random1+random2;
+
+
+
+
+
+
         if(random3==1)
         {
             btn1.setText(Integer.toString(sum));
@@ -72,44 +93,52 @@ public class Test_Activity extends AppCompatActivity {
         {
             case R.id.button1:
                 if(Integer.parseInt(String.valueOf(btn1.getText()))==sum) {
-                    answer.setText("Correct Answer");
+                    myMessage.setText("Correct Answer");
+                    myToast.show();
                     btn1.setBackgroundColor(Color.GREEN);
                     count++;
                 } else {
-                    answer.setText("Wrong Answer");
+                    myMessage.setText("Wrong Answer");
+                    myToast.show();
                     btn1.setBackgroundColor(Color.RED);
                 }
                 break;
             case R.id.button2:
                 if(Integer.parseInt(String.valueOf(btn2.getText()))==sum)
                 {
-                    answer.setText("Correct Answer");
+                    myMessage.setText("Correct Answer");
+                    myToast.show();
                     btn2.setBackgroundColor(Color.GREEN);
                     count++;
                 } else {
-                    answer.setText("Wrong Answer");
+                    myMessage.setText("Wrong Answer");
+                    myToast.show();
                     btn2.setBackgroundColor(Color.RED);
                 }
                 break;
             case R.id.button3:
                 if(Integer.parseInt(String.valueOf(btn3.getText()))==sum)
                 {
-                    answer.setText("Correct Answer");
+                    myMessage.setText("Correct Answer");
+                    myToast.show();
                     btn3.setBackgroundColor(Color.GREEN);
                     count++;
                 } else {
-                    answer.setText("Wrong Answer");
+                    myMessage.setText("Wrong Answer");
+                    myToast.show();
                     btn3.setBackgroundColor(Color.RED);
                 }
                 break;
             case R.id.button4:
                 if(Integer.parseInt(String.valueOf(btn4.getText()))==sum)
                 {
-                    answer.setText("Correct Answer");
+                    myMessage.setText("Correct Answer");
+                    myToast.show();
                     btn4.setBackgroundColor(Color.GREEN);
                     count++;
                 } else {
-                    answer.setText("Wrong Answer");
+                    myMessage.setText("Wrong Answer");
+                    myToast.show();
                     btn4.setBackgroundColor(Color.RED);
                 }
                 break;
