5712031292c7d6ec6b39919c0f04f3f6a8c720c4
Hamid-mian <iqbalhamid380@gmail.com>
2022-06-05 22:29:01 -0800

    Complete application using toast,alert box,customized list view

--- app/src/main/java/com/example/test_activity/learningActivity.java
+++ app/src/main/java/com/example/test_activity/learningActivity.java
@@ -46,20 +46,15 @@ public class learningActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (position ==  0) {
                     builder = new AlertDialog.Builder(learningActivity.this);
-                    builder.setMessage("About Face book");
+                    builder.setMessage(" Facebook's mission is to give people the power to build" +
+                            " community and bring the world closer together. People use Facebook " +
+                            "to stay connected with friends and family, to discover what's going on " +
+                            "in the world, and to share and express what matters to them.");
                     builder.setTitle("FaceBook Description");
                     builder.setCancelable(false);
-                    builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int asdf)
-                        {
-                            finish();
-                        }
-                    });
-                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
                         }
                     });
@@ -67,20 +62,15 @@ public class learningActivity extends AppCompatActivity {
                     alertDialog.show();               }
                 if (position ==  1) {
                     builder = new AlertDialog.Builder(learningActivity.this);
-                    builder.setMessage("Do You want to change Image");
+                    builder.setMessage("WhatsApp uses the internet to send messages, images, audio " +
+                            "or video. The service is very similar to text messaging services," +
+                            " however, because WhatsApp uses the internet to send messages, the cost " +
+                            "of using WhatsApp is significantly less than texting.");
                     builder.setTitle("Whatsapp Description");
                     builder.setCancelable(false);
-                    builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int asdf)
-                        {
-                            finish();
-                        }
-                    });
-                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
                         }
                     });
@@ -89,20 +79,14 @@ public class learningActivity extends AppCompatActivity {
                 if (position ==  2) {
 
                     builder = new AlertDialog.Builder(learningActivity.this);
-                    builder.setMessage("Do You want to change Image");
+                    builder.setMessage("Twitter is an American microblogging and social networking" +
+                            " service on which users post and interact with messages known as" +
+                            " tweets.");
                     builder.setTitle("Twitter Description");
                     builder.setCancelable(false);
-                    builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int asdf)
-                        {
-                            finish();
-                        }
-                    });
-                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
                         }
                     });
@@ -111,20 +95,15 @@ public class learningActivity extends AppCompatActivity {
                 if (position ==  3) {
 
                     builder = new AlertDialog.Builder(learningActivity.this);
-                    builder.setMessage("Do You want to change Image");
+                    builder.setMessage("Instagram was started in San Francisco by Kevin Systrom and " +
+                            "Mike Krieger, who initially tried creating a platform similar to " +
+                            "Foursquare but then turned their attention exclusively to photo sharing. " +
+                            "The word Instagram is an amalgam of instant camera and telegram.");
                     builder.setTitle("Instagram Description");
                     builder.setCancelable(false);
-                    builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int asdf)
-                        {
-                            finish();
-                        }
-                    });
-                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
                         }
                     });
@@ -132,20 +111,14 @@ public class learningActivity extends AppCompatActivity {
                     alertDialog.show();             }
                 if (position ==  4) {
                     builder = new AlertDialog.Builder(learningActivity.this);
-                    builder.setMessage("Do You want to change Image");
+                    builder.setMessage("YouTube is a video sharing service where users can watch, like," +
+                            " share, comment and upload their own videos. The video service can be " +
+                            "accessed on PCs, laptops, tablets and via mobile phones.");
                     builder.setTitle("Youtube Description");
                     builder.setCancelable(false);
-                    builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int asdf)
-                        {
-                            finish();
-                        }
-                    });
-                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
                         }
                     });
@@ -153,20 +126,15 @@ public class learningActivity extends AppCompatActivity {
                     alertDialog.show();}
                     if (position ==  5) {
                         builder = new AlertDialog.Builder(learningActivity.this);
-                        builder.setMessage("Do You want to change Image");
+                        builder.setMessage("LinkedIn is a social networking website designed for business" +
+                                " professionals. It allows you to share work-related information with " +
+                                "other users and keep an online list of professional contacts. Like" +
+                                " Facebook and MySpace, LinkedIn allows you to create a custom profile.");
                         builder.setTitle("Linkedin Description");
                         builder.setCancelable(false);
-                        builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                        builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialog, int asdf)
-                            {
-                                finish();
-                            }
-                        });
-                        builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which)
-                            {
+                            public void onClick(DialogInterface dialog, int which) {
                                 dialog.cancel();
                             }
                         });
@@ -174,20 +142,14 @@ public class learningActivity extends AppCompatActivity {
                         alertDialog.show();}
                         if (position ==  6) {
                             builder = new AlertDialog.Builder(learningActivity.this);
-                            builder.setMessage("Do You want to change Image");
+                            builder.setMessage("GitHub is a distributed version-control platform where" +
+                                    " users can collaborate on or adopt open source code projects," +
+                                    " fork code, share ideas and more.");
                             builder.setTitle("Github Description");
                             builder.setCancelable(false);
-                            builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                                 @Override
-                                public void onClick(DialogInterface dialog, int asdf)
-                                {
-                                    finish();
-                                }
-                            });
-                            builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which)
-                                {
+                                public void onClick(DialogInterface dialog, int which) {
                                     dialog.cancel();
                                 }
                             });
@@ -195,20 +157,16 @@ public class learningActivity extends AppCompatActivity {
                             alertDialog.show();}
                             if (position ==  7) {
                                 builder = new AlertDialog.Builder(learningActivity.this);
-                                builder.setMessage("Do You want to change Image");
+                                builder.setMessage("What is TikTok? TikTok is a social media platform" +
+                                        " for creating, sharing and discovering short videos. The app is" +
+                                        " used by young people as an outlet to express themselves through " +
+                                        "singing, dancing, comedy, and lip-syncing, and allows users to create " +
+                                        "videos and share them across a community.");
                                 builder.setTitle("Tiktok Description");
                                 builder.setCancelable(false);
-                                builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                                builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                                     @Override
-                                    public void onClick(DialogInterface dialog, int asdf)
-                                    {
-                                        finish();
-                                    }
-                                });
-                                builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which)
-                                    {
+                                    public void onClick(DialogInterface dialog, int which) {
                                         dialog.cancel();
                                     }
                                 });
@@ -216,20 +174,15 @@ public class learningActivity extends AppCompatActivity {
                                 alertDialog.show();}
                                 if (position ==  8) {
                                     builder = new AlertDialog.Builder(learningActivity.this);
-                                    builder.setMessage("Do You want to change Image");
+                                    builder.setMessage("Viber is a VoIP and instant messaging application with" +
+                                            " cross-platform capabilities that allows users to exchange audio and" +
+                                            " video calls, stickers, group chats, and instant voice and video" +
+                                            " messages.");
                                     builder.setTitle("viber Description");
                                     builder.setCancelable(false);
-                                    builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                                         @Override
-                                        public void onClick(DialogInterface dialog, int asdf)
-                                        {
-                                            finish();
-                                        }
-                                    });
-                                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which)
-                                        {
+                                        public void onClick(DialogInterface dialog, int which) {
                                             dialog.cancel();
                                         }
                                     });
@@ -237,16 +190,13 @@ public class learningActivity extends AppCompatActivity {
                                     alertDialog.show();}
                                     if (position ==  9) {
                                         builder = new AlertDialog.Builder(learningActivity.this);
-                                        builder.setMessage("Do You want to change Image");
+                                        builder.setMessage("Google is an internet search engine. It uses a " +
+                                                "proprietary algorithm that's designed to retrieve and order search " +
+                                                "results to provide the most relevant and dependable sources of data" +
+                                                " possible.");
                                         builder.setTitle("Google Description");
                                         builder.setCancelable(false);
-                                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int asdf) {
-                                                finish();
-                                            }
-                                        });
-                                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                        builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
                                                 dialog.cancel();
