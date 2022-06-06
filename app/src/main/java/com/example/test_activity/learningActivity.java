81f4b3f79e1ea75dad7eaa078213bb50c0ecbd76
Hamid-mian <iqbalhamid380@gmail.com>
2022-06-05 18:47:07 -0800

    Custom list view of social apps learning

--- app/src/main/java/com/example/test_activity/learningActivity.java
+++ app/src/main/java/com/example/test_activity/learningActivity.java
@@ -1,13 +1,73 @@
 package com.example.test_activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class learningActivity extends AppCompatActivity {
+
+    ListView listView;
+    String mTitle[] = {"Facebook", "Whatsapp", "Twitter", "Instagram", "Youtube","Linkedin", "Github", "Tiktok", "Viber", "Google"};
+    String mDescription[] = {"Facebook Description", "Whatsapp Description", "Twitter Description", "Instagram Description", "Youtube Description","Linkedin Description", "Github Description", "Tiktok Description", "Viber Description", "Google Description"};
+    int images[] = {R.drawable.facebook, R.drawable.whatsapp, R.drawable.twitter, R.drawable.instagram, R.drawable.youtube,R.drawable.linkdin, R.drawable.github, R.drawable.tiktok, R.drawable.viber, R.drawable.google};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+
+        listView = findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, images);
+        listView.setAdapter(adapter);
+
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter (Context c, String title[], String description[], int imgs[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+            return row;
+        }
     }
 }
